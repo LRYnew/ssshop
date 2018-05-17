3cc0dcc83710a215b16968f8a24d91627e2f3534
YJob <472830294@qq.com>
2018-05-17 17:58:27 +0800

    2018-05-17

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -4,11 +4,14 @@ from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Goods
 from .serializers import GoodsListSerializers
 from .filters import GoodsFilter
+
+
 # Create your views here.
 
 
@@ -26,8 +29,12 @@ class GoodsListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     商品列表
     """
-    queryset  = Goods.objects.all()
+    queryset = Goods.objects.all()
     serializer_class = GoodsListSerializers
     pagination_class = LargeResultsSetPagination
     # filter_fields = ('name', 'shop_price')
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     filter_class = GoodsFilter
+    search_fields = ['name', 'goods_sn', 'goods_desc', 'goods_brief']
+    ordering_fields = ('name', 'sold_num', 'add_time', 'update_time')
+    ordering = ('name',)
